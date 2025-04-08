c9110935c2ffb6e9b4a3cd32a262def916cbc5bd
Zotov.I <ivan151698@gmail.com>
2025-04-08 12:31:47 +0300

    product page test in progress

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,9 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    REGISTER_FORM = (By.ID, "register_form")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner")
---
+++ pages/product_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        button.click()
+
+    def should_be_message_about_adding(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), (
+            "Product name is not presented")
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), (
+            "Message about adding is not presented")
+
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING).text
+        assert product_name in message, "No product name in the message"
+
+
